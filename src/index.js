83eb5db76dcc47c15d5122ef278eb281075790a2
grimpatron <hellhammer220bpm@gmail.com>
2024-08-20 01:58:23 +0300

    feat: finish towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-module.exports = function towelSort (matrix) {
-  let res = [];
-  // if (matrix.length == 0) {
-  //   console.log("pusto");
-  //   return [];
-  // }
-  console.log(matrix);
-  if (!matrix) {
-    return res;
-  }
-  for (let i = 0; i < matrix.length; i++) {
-    if (i % 2 == 0) {
-      res = res.concat(matrix[i]);
-    } else if (i % 2 == 1){
-      res = res.concat(matrix[i].reverse());
+// module.exports = function towelSort(matrix) {
+//     const result = [];
+
+//     if (!matrix) return result;
+
+//     for (let i = 0; i < matrix.length; i++) {
+//         if (i % 2 == 0) {
+//             for (; matrix[i].length > 0; ) {
+//                 result.push(matrix[i].shift());
+//             }
+//         } else {
+//             for (let j = 0; matrix[i].length > 0; j++)
+//                 result.push(matrix[i].pop());
+//         }
+//     }
+//     return result;
+// };
+
+module.exports = function towelSort(matrix) {
+    let result = [];
+    if (!matrix) return result;
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 == 0) {
+            result = result.concat(matrix[i]);
+        } else if (i % 2 == 1) {
+            result = result.concat(matrix[i].reverse());
+        }
     }
-  }
-  return res;
-}
+    return result;
+};
